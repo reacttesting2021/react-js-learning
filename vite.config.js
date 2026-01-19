63ac80de26af5d823830dc08aff2009ad7aa8688
Bhairav Singh <krishna@Bhairavs-MacBook.local>
2026-01-19 08:29:37 +0530

    Add my Learning first commit files

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,   // change to any port you want
+    strictPort: true // optional: fail if port is already in use
+  }
+})
