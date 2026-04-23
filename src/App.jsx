07a83dc11ea7b5c2e5c7264d031b9fcce9910b7a
Bhairav Singh <krishna@Bhairavs-MacBook.local>
2026-04-23 17:18:47 +0530

    Changed new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Tailwindcss from './components/Tailwindcss.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-  <h1> Learn React Js First App</h1>
-  <h2>Hello Bhairav How are you, Now your first code now deployed.</h2>
-  </>
+      <h1> Learn React Js First App</h1>
+      <h2>Hello Bhairav How are you, Now your first code now deployed.</h2>
+      <Tailwindcss />
+    </>
 )
 }
 
---
+++ src/components/Tailwindcss.jsx
@@ -0,0 +1,11 @@
+
+function Tailwindcss() {
+
+  return (
+    <>
+      <h1> Implemented Tailwind CSS</h1>
+    </>
+)
+}
+
+export default Tailwindcss
